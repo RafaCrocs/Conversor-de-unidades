c3a28bb78067f93818e4618c6cfa2a56fd600536
Rafa Cruz <jose.cruz23@ulatina.net>
2025-11-29 19:38:47 -0600

    Intentado Hacerlo dinamico 1

---
+++ CU_Clases/CU_Clases/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CU_Clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMenu();
+        }
+
+        static void MostrarMenu()
+        {
+            Console.WriteLine("============================================");
+            Console.WriteLine("========   CONVERSOR DE UNIDADES   =========");
+            Console.WriteLine("============================================\n");
+            Console.WriteLine("Ingrese la unidad de origen");
+
+
+
+        }
+        static int RevisionNums()
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int numero))
+                {
+                    return numero;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada invalida. Por favor ingrese un numero entero.");
+                }
+            }
+        }
+    }
+}
---
+++ CU_Clases/CU_Clases/Unidades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CU_Clases
+{
+    internal class Unidades
+    {
+
+        public int IdUnidad { get; set; }
+        public string NombreUnidad { get; set; }
+        public List<Conversion> Conversiones { get; set; }
+    }
+}
